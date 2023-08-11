310b63c9f96faef8951d23e1dc6c35fa3b523e6b
Patrik Kozak <patrik@trbl.design>
2023-08-11 12:52:06 -0400

    feat: updates link field component

--- src/fields/link.ts
+++ src/fields/link.ts
@@ -3,9 +3,9 @@ import type { Field, GroupField } from 'payload/types'
 import deepMerge from '../utilities/deepMerge'
 
 export const appearanceOptions = {
-  default: {
-    label: 'Default',
-    value: 'default',
+  primary: {
+    label: 'Primary Button',
+    value: 'primary',
   },
   secondary: {
     label: 'Secondary Button',
@@ -13,7 +13,7 @@ export const appearanceOptions = {
   },
 }
 
-export type LinkAppearances = 'default' | 'secondary'
+export type LinkAppearances = 'primary' | 'secondary'
 
 type LinkType = (options?: {
   appearances?: LinkAppearances[] | false
@@ -34,11 +34,21 @@ const link: LinkType = ({ appearances, disableLabel = false, overrides = {} } = 
         type: 'row',
         fields: [
           {
-            name: 'url',
-            label: 'Custom URL',
-            type: 'text',
-            required: true,
+            name: 'type',
+            type: 'radio',
+            options: [
+              {
+                label: 'Internal link',
+                value: 'reference',
+              },
+              {
+                label: 'Custom URL',
+                value: 'custom',
+              },
+            ],
+            defaultValue: 'reference',
             admin: {
+              layout: 'horizontal',
               width: '50%',
             },
           },
@@ -58,20 +68,54 @@ const link: LinkType = ({ appearances, disableLabel = false, overrides = {} } = 
     ],
   }
 
-  if (!disableLabel) {
-    linkResult.fields.push({
-      name: 'label',
-      label: 'Label',
+  let linkTypes: Field[] = [
+    {
+      name: 'reference',
+      label: 'Document to link to',
+      type: 'relationship',
+      relationTo: ['pages', 'projects'],
+      required: true,
+      maxDepth: 1,
+      admin: {
+        condition: (_, siblingData) => siblingData?.type === 'reference',
+      },
+    },
+    {
+      name: 'url',
+      label: 'Custom URL',
       type: 'text',
       required: true,
       admin: {
-        width: '50%',
+        condition: (_, siblingData) => siblingData?.type === 'custom',
       },
+    },
+  ]
+
+  if (!disableLabel) {
+    linkTypes[0].admin.width = '50%'
+    linkTypes[1].admin.width = '50%'
+
+    linkResult.fields.push({
+      type: 'row',
+      fields: [
+        ...linkTypes,
+        {
+          name: 'label',
+          label: 'Label',
+          type: 'text',
+          required: true,
+          admin: {
+            width: '50%',
+          },
+        },
+      ],
     })
+  } else {
+    linkResult.fields = [...linkResult.fields, ...linkTypes]
   }
 
   if (appearances !== false) {
-    let appearanceOptionsToUse = [appearanceOptions.default, appearanceOptions.secondary]
+    let appearanceOptionsToUse = [appearanceOptions.primary, appearanceOptions.secondary]
 
     if (appearances) {
       appearanceOptionsToUse = appearances.map(appearance => appearanceOptions[appearance])
@@ -80,7 +124,7 @@ const link: LinkType = ({ appearances, disableLabel = false, overrides = {} } = 
     linkResult.fields.push({
       name: 'appearance',
       type: 'select',
-      defaultValue: 'default',
+      defaultValue: 'primary',
       options: appearanceOptionsToUse,
       admin: {
         description: 'Choose how the link should be rendered.',
