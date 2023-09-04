4a19b74157e656056b59ee1197fbce8204da93d0
Austin Montoya <austin.r.montoya@gmail.com>
2023-09-03 21:29:25 -0700

    clean up env vars, log image domains during build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ if (process.env.PAYLOAD_PUBLIC_SERVER_URL) {
   imageDomains.push(process.env.PAYLOAD_PUBLIC_SERVER_URL)
 }
 
+// eslint-disable-next-line no-console
+console.log('allowed image domains:', imageDomains.join(', '))
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
