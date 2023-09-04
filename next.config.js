056e35e68b2c617ee54cb3a55f0dcbd6f386d7a6
Austin Montoya <austin.r.montoya@gmail.com>
2023-09-03 21:21:22 -0700

    use payload URI for images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
 const imageDomains = ['localhost']
-if (process.env.NEXT_PUBLIC_SERVER_URL) {
-  imageDomains.push(process.env.NEXT_PUBLIC_SERVER_URL)
+if (process.env.PAYLOAD_PUBLIC_SERVER_URL) {
+  imageDomains.push(process.env.PAYLOAD_PUBLIC_SERVER_URL)
 }
 
 module.exports = {
