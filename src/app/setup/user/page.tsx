f9c03abc0cd5a5fd80c3dd32b8a21d56a3367649
Austin Montoya <austin@liminal.sh>
2024-07-06 23:56:21 -0700

    redirect if user already exists

--- src/app/setup/user/page.tsx
+++ src/app/setup/user/page.tsx
@@ -1,6 +1,17 @@
+import { getPayload } from '@/utilities/api'
 import { CreateUserForm } from './form'
+import { redirect } from 'next/navigation'
 
-export default function SetupUserPage() {
+export default async function SetupUserPage() {
+  const payload = await getPayload()
+  const result = await payload.find({
+    collection: 'users',
+  })
+
+  if (result.totalDocs > 0) {
+    redirect('/setup/content')
+  }
+
   return (
     <div className="mx-auto w-2/3 max-w-screen-md">
       <h1 className="text-3xl font-medium">Create a User</h1>
