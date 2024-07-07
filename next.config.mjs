6d42039258a58d03936babdb33ce5c626e14d3f5
Austin Montoya <austin@liminal.sh>
2024-07-07 15:56:58 -0700

    add remote image domains

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,14 @@ const nextConfig = {
   // Your Next.js config here
   experimental: {
     reactCompiler: false,
-  }
+  },
+  images: {
+    domains: ['localhost'].concat(
+      process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL 
+        ? [process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL] 
+        : []
+    )
+  },
 }
 
 export default withPayload(nextConfig)
